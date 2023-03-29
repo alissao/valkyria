7ea75b3c3a9c332dd707e2e55deba1506105256b
alissao <alissonhbv@gmail.com>
2023-03-29 13:39:09 -0300

    Spring initialzr.

---
+++ src/main/java/com/cartorio/valkyria/ValkyriaApplication.java
@@ -0,0 +1,13 @@
+package com.cartorio.valkyria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValkyriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValkyriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartorio/valkyria/ValkyriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartorio.valkyria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValkyriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
