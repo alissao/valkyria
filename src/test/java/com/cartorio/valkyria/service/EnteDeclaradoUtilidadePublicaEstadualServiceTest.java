02aa944fd00446a2ee3238540c841fc2c1ec9f14
alissao <alissonhbv@gmail.com>
2023-04-03 23:30:37 -0300

    Added test case for finding EnteDeclarado by name.

--- src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
+++ src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
@@ -1,13 +1,16 @@
 package com.cartorio.valkyria.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -71,6 +74,38 @@ public class EnteDeclaradoUtilidadePublicaEstadualServiceTest {
 
     // Assert
     assertTrue(optionalOfEntity.isPresent());
+  }
+
+  @Test
+  void should_find_and_return_list_of_enteDeclarados() {
+    // Arrange
+    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclaradoEntity = new EnteDeclaradoUtilidadePublicaEstadualEntity();
+    enteDeclaradoEntity.setCdComarca(1);
+    enteDeclaradoEntity.setCdEntePub(1L);
+    enteDeclaradoEntity.setDtLei("01/04/2023");
+    enteDeclaradoEntity.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Tres");
+    enteDeclaradoEntity.setNomeComarca("Comarca Valkyrias");
+    enteDeclaradoEntity.setNomeComarcaEntidade("Comarca Valkyrias Vikings");
+    enteDeclaradoEntity.setNomeEntidade("Vikings");
+    enteDeclaradoEntity.setLei("Lei numero um");
+    
+    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclaradoEntity2 = new EnteDeclaradoUtilidadePublicaEstadualEntity();
+    enteDeclaradoEntity2.setCdComarca(2);
+    enteDeclaradoEntity2.setCdEntePub(2L);
+    enteDeclaradoEntity2.setDtLei("01/04/2024");
+    enteDeclaradoEntity2.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Quatro");
+    enteDeclaradoEntity2.setNomeComarca("Comarca Valkyrias 2");
+    enteDeclaradoEntity2.setNomeComarcaEntidade("Comarca Valkyrias Vikings Segunda Divisao");
+    enteDeclaradoEntity2.setNomeEntidade("Vikings 2");
+    enteDeclaradoEntity2.setLei("Lei numero dois");
+    
+    when(mockRepository.findByNomeEntidade(anyString())).thenReturn(List.of(enteDeclaradoEntity, enteDeclaradoEntity2));
+
+    // Act
+    var listOfEntes = mockService.findByNomeEntidade(anyString());
+
+    // Assert
+    assertFalse(listOfEntes.isEmpty());
   }
   
 }
