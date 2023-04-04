a63481ae0ecbf51a58ffb5340a02ec912c8033a9
alissao <alissonhbv@gmail.com>
2023-04-04 01:59:23 -0300

    Changing test to adapt new signature.

--- src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
+++ src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
@@ -1,8 +1,8 @@
 package com.cartorio.valkyria.service;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -21,6 +21,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import com.cartorio.valkyria.model.entesDeclaradosUtilidadePublicaEstadual.EnteDeclaradoUtilidadePublicaEstadualEntity;
 import com.cartorio.valkyria.model.tjClient.TjWebServiceClient;
@@ -79,33 +81,38 @@ public class EnteDeclaradoUtilidadePublicaEstadualServiceTest {
   @Test
   void should_find_and_return_list_of_enteDeclarados() {
     // Arrange
-    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclaradoEntity = new EnteDeclaradoUtilidadePublicaEstadualEntity();
-    enteDeclaradoEntity.setCdComarca(1);
-    enteDeclaradoEntity.setCdEntePub(1L);
-    enteDeclaradoEntity.setDtLei("01/04/2023");
-    enteDeclaradoEntity.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Tres");
-    enteDeclaradoEntity.setNomeComarca("Comarca Valkyrias");
-    enteDeclaradoEntity.setNomeComarcaEntidade("Comarca Valkyrias Vikings");
-    enteDeclaradoEntity.setNomeEntidade("Vikings");
-    enteDeclaradoEntity.setLei("Lei numero um");
+    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclarado = new EnteDeclaradoUtilidadePublicaEstadualEntity();
+    enteDeclarado.setCdComarca(1);
+    enteDeclarado.setCdEntePub(1L);
+    enteDeclarado.setDtLei("01/04/2023");
+    enteDeclarado.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Tres");
+    enteDeclarado.setNomeComarca("Comarca Valkyrias");
+    enteDeclarado.setNomeComarcaEntidade("Comarca Valkyrias Vikings");
+    enteDeclarado.setNomeEntidade("Vikings");
+    enteDeclarado.setLei("Lei numero um");
     
-    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclaradoEntity2 = new EnteDeclaradoUtilidadePublicaEstadualEntity();
-    enteDeclaradoEntity2.setCdComarca(2);
-    enteDeclaradoEntity2.setCdEntePub(2L);
-    enteDeclaradoEntity2.setDtLei("01/04/2024");
-    enteDeclaradoEntity2.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Quatro");
-    enteDeclaradoEntity2.setNomeComarca("Comarca Valkyrias 2");
-    enteDeclaradoEntity2.setNomeComarcaEntidade("Comarca Valkyrias Vikings Segunda Divisao");
-    enteDeclaradoEntity2.setNomeEntidade("Vikings 2");
-    enteDeclaradoEntity2.setLei("Lei numero dois");
+    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclarado2 = new EnteDeclaradoUtilidadePublicaEstadualEntity();
+    enteDeclarado2.setCdComarca(2);
+    enteDeclarado2.setCdEntePub(2L);
+    enteDeclarado2.setDtLei("01/04/2024");
+    enteDeclarado2.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Quatro");
+    enteDeclarado2.setNomeComarca("Comarca Valkyrias 2");
+    enteDeclarado2.setNomeComarcaEntidade("Comarca Valkyrias Vikings Segunda Divisao");
+    enteDeclarado2.setNomeEntidade("Vikings 2");
+    enteDeclarado2.setLei("Lei numero dois");
     
-    when(mockRepository.findByNomeEntidade(anyString())).thenReturn(List.of(enteDeclaradoEntity, enteDeclaradoEntity2));
+    when(mockRepository.findByNomeEntidade(anyString(), PageRequest.of(2, 1)))
+        .thenReturn(new PageImpl<EnteDeclaradoUtilidadePublicaEstadualEntity>(
+          List.of(enteDeclarado, enteDeclarado2),
+          PageRequest.of(2, 1),
+          2
+        ));
 
     // Act
-    var listOfEntes = mockService.findByNomeEntidade(anyString());
+    var listOfEntes = mockService.findByNomeEntidade(anyString(), PageRequest.of(2, 1));
 
     // Assert
-    assertFalse(listOfEntes.isEmpty());
+    assertFalse(listOfEntes.getContent().isEmpty());
   }
   
 }
