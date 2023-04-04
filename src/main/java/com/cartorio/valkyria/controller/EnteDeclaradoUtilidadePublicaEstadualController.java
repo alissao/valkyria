5f4ece2712874406a1971633b218cef40e513891
alissao <alissonhbv@gmail.com>
2023-04-04 02:45:27 -0300

    Added paginated endpoint for querying EnteDeclarado's by name.

--- src/main/java/com/cartorio/valkyria/controller/EnteDeclaradoUtilidadePublicaEstadualController.java
+++ src/main/java/com/cartorio/valkyria/controller/EnteDeclaradoUtilidadePublicaEstadualController.java
@@ -1,10 +1,14 @@
 package com.cartorio.valkyria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cartorio.valkyria.model.utility.CustomMapper;
@@ -28,12 +32,10 @@ public class EnteDeclaradoUtilidadePublicaEstadualController {
         .orElse(ResponseEntity.notFound().build());
   }
 
-  // @GetMapping("/nome-entidade/{name}")
-  // public ResponseEntity<EnteDeclaradoUtilidadePublicaEstadual> findByNomeEnte(@PathVariable String name) {
-  //   return service.findByNomeEntidade(name)
-  //       .map(entity -> ResponseEntity.ok(mapper.map(entity, EnteDeclaradoUtilidadePublicaEstadual.class)))
-  //       .orElse(ResponseEntity.notFound().build());
-  // }
+  @GetMapping("/v1/nome-entidade/{name}")
+  public ResponseEntity<Page<EnteDeclaradoUtilidadePublicaEstadual>> findByNomeEnte(@PathVariable String name, @RequestParam int page) {
+    return ResponseEntity.ok(service.findByNomeEntidade(name, PageRequest.of(page, 5)));
+  }
 
   
 }
