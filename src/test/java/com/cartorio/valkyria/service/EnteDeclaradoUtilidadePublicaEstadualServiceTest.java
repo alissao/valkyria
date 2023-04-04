0c503082de78ae442e734a49725beaa18a7ca8bb
alissao <alissonhbv@gmail.com>
2023-04-03 23:21:27 -0300

    Added test to find EnteDeclarado by Id

--- src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
+++ src/test/java/com/cartorio/valkyria/service/EnteDeclaradoUtilidadePublicaEstadualServiceTest.java
@@ -1,9 +1,14 @@
 package com.cartorio.valkyria.service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -14,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cartorio.valkyria.model.entesDeclaradosUtilidadePublicaEstadual.EnteDeclaradoUtilidadePublicaEstadualEntity;
 import com.cartorio.valkyria.model.tjClient.TjWebServiceClient;
 import com.cartorio.valkyria.repository.EnteDeclaradoUtilidadePublicaEstadualRepository;
 import com.cartorio.valkyria.wsdl.EnteDeclaradoUtilidadePublicaEstadual;
@@ -43,6 +49,28 @@ public class EnteDeclaradoUtilidadePublicaEstadualServiceTest {
     // Assert
     verify(mockRepository, times(1)).count();
     verifyNoMoreInteractions(mockRepository);
+  }
+
+
+  @Test
+  void should_find_and_return_one_enteDeclarado() {
+    // Arrange
+    EnteDeclaradoUtilidadePublicaEstadualEntity enteDeclaradoEntity = new EnteDeclaradoUtilidadePublicaEstadualEntity();
+    enteDeclaradoEntity.setCdComarca(1);
+    enteDeclaradoEntity.setCdEntePub(1L);
+    enteDeclaradoEntity.setDtLei("01/04/2023");
+    enteDeclaradoEntity.setDtLeiExtenso("Primeiro de Abril de Dois Mil e Vinte e Tres");
+    enteDeclaradoEntity.setNomeComarca("Comarca Valkyrias");
+    enteDeclaradoEntity.setNomeComarcaEntidade("Comarca Valkyrias Vikings");
+    enteDeclaradoEntity.setNomeEntidade("Vikings");
+    enteDeclaradoEntity.setLei("Lei numero um");
+    when(mockRepository.findByCdEntePub(anyLong())).thenReturn(Optional.of(enteDeclaradoEntity));
+
+    // Act
+    var optionalOfEntity = mockService.findByCdEntePub(1L);
+
+    // Assert
+    assertTrue(optionalOfEntity.isPresent());
   }
   
 }
